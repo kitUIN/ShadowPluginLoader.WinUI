bddcdd26ed14b7e0e35cd7154382b062cca0fbee
Nacho <kulujun@gmail.com>
2024-04-15 04:08:57 +0800

    fix .csproj

--- ShadowPluginLoader.WinUI/Models/SortPluginData.cs
+++ ShadowPluginLoader.WinUI/Models/SortPluginData.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 
 namespace ShadowPluginLoader.WinUI.Models
 {
+    /// <summary>
+    /// SortPluginData
+    /// </summary>
     public class SortPluginData
     {
+        /// <summary>
+        /// Plugin Requires
+        /// </summary>
         public string[] Requires { get;}
+        /// <summary>
+        /// Plugin Id
+        /// </summary>
         public string Id { get; }
+        /// <summary>
+        /// Plugin Type
+        /// </summary>
         public Type? PluginType { get; set; }
+        /// <summary>
+        /// SortPluginData
+        /// </summary>
+        /// <param name="id">Plugin Id</param>
+        /// <param name="req">Requires</param>
         public SortPluginData(string id ,string[] req) 
         {
             Id = id;
