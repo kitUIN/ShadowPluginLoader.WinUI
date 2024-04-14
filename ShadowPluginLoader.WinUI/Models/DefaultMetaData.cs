0ac6694075bbc86adb1c933e99ac747c1a8ff1c4
Nacho <kulujun@gmail.com>
2024-04-15 05:08:38 +0800

    Add DefaultPluginMetaData

---
+++ ShadowPluginLoader.WinUI/Models/DefaultMetaData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ShadowPluginLoader.WinUI.Interfaces;
+
+namespace ShadowPluginLoader.WinUI.Models;
+
+/// <summary>
+/// Default PluginMetaData
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class DefaultPluginMetaData: Attribute, IPluginMetaData
+{
+    /// <summary>
+    /// <inheritdoc cref="IPluginMetaData.Id"/>
+    /// </summary>
+    public string Id { get; }
+    /// <summary>
+    /// <inheritdoc cref="IPluginMetaData.Name"/>
+    /// </summary>
+    public string Name { get; }
+    /// <summary>
+    /// <inheritdoc cref="IPluginMetaData.Version"/>
+    /// </summary>
+    public string Version { get; }
+    /// <summary>
+    /// <inheritdoc cref="IPluginMetaData.Requires"/>
+    /// </summary>
+    public string[] Requires { get; }
+
+    /// <summary>
+    /// Default PluginMetaData
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="name"></param>
+    /// <param name="version"></param>
+    /// <param name="requires"></param>
+    public DefaultPluginMetaData(string id, string name, string version, string[] requires)
+    {
+        Id = id;
+        Name = name;
+        Version = version;
+        Requires = requires;
+    }
+}
