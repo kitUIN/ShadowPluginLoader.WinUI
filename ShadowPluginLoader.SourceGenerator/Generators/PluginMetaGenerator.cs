f588942b876dcd8c6fec267fdcabe32097ada62d
Nacho <kulujun@gmail.com>
2024-04-23 17:23:52 +0800

    using Newtonsoft.Json

--- ShadowPluginLoader.SourceGenerator/Generators/PluginMetaGenerator.cs
+++ ShadowPluginLoader.SourceGenerator/Generators/PluginMetaGenerator.cs
@@ -1,5 +1,5 @@
-﻿using System.Text.Json.Nodes;
-using System.Xml.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
@@ -8,8 +8,8 @@ namespace ShadowPluginLoader.SourceGenerator.Generators;
 [Generator]
 internal class PluginMetaGenerator : ISourceGenerator
 {
-    private static JsonNode? PluginNode;
-    private static JsonNode? PluginDNode;
+    private static JObject? PluginNode;
+    private static JObject? PluginDNode;
     static void GetJson(GeneratorExecutionContext context)
     {
         foreach (var file in context.AdditionalFiles)
@@ -22,7 +22,7 @@ internal class PluginMetaGenerator : ISourceGenerator
                 {
                     throw new Exception($"Not Found plugin.json");
                 }
-                PluginNode = JsonNode.Parse(jsonString.ToString());
+                PluginNode = JObject.Parse(jsonString.ToString());
                 if(PluginNode is null)
                 {
                     throw new Exception($"plugin.json Is Empty");
@@ -35,97 +35,13 @@ internal class PluginMetaGenerator : ISourceGenerator
                 {
                     throw new Exception($"Not Found plugin.d.json");
                 }
-                PluginDNode = JsonNode.Parse(jsonString.ToString());
+                PluginDNode = JObject.Parse(jsonString.ToString());
                 if(PluginDNode is null)
                 {
                     throw new Exception($"plugin.d.json Is Empty");
                 }
             }
         }
-    }
-
-    static string GetMeta(GeneratorExecutionContext context)
-    {
-        foreach (AdditionalText file in context.AdditionalFiles)
-        {
-            if (Path.GetExtension(file.Path).Equals(".csproj", StringComparison.OrdinalIgnoreCase))
-            {
-                var doc = XDocument.Load(file.Path);
-                var fileName = Path.GetFileName(file.Path);
-                var meta = new MetaYaml();
-                if (doc.Root.Element("PropertyGroup") is { } propertyGroup)
-                {
-                    if (propertyGroup.Element("Version") is { } version)
-                    {
-                        if (version.Value.Split('.').Length != 4)
-                            throw new Exception(
-                                $"{fileName}文件中插件元数据版本号必须为<Version>{{Major}}.{{Minor}}.{{Build}}.{{Revision}}<Version/>");
-                        meta.Version = version.Value;
-                    }
-                    else
-                        throw new Exception($"{fileName}文件中缺少插件元数据<Version><Version/>");
-
-                    if (propertyGroup.Element("PackageId") is { } id)
-                        meta.Id = id.Value.Replace("ShadowViewer.Plugin.", "");
-                    else
-                        throw new Exception(
-                            @$"{fileName}文件中缺少插件元数据<PackageId>ShadowViewer.Plugin.{{插件id}}<PackageId/>");
-                    if (propertyGroup.Element("PluginLogo") is { } logo)
-                        meta.Logo = logo.Value;
-                    else
-                        meta.Logo = "";
-                    if (propertyGroup.Element("PluginName") is { } name)
-                        meta.Name = name.Value;
-                    else
-                        throw new Exception($"{fileName}文件中缺少插件元数据<PluginName><PluginName/>");
-
-                    if (propertyGroup.Element("PluginLang") is { } lang)
-                    {
-                        var temp = lang.Value.Split(';');
-                        for (int i = 0; i < temp.Length; i++)
-                        {
-                            temp[i] = @$"""{temp[i]}""";
-                        }
-
-                        meta.Lang = temp;
-                    }
-                    else
-                        meta.Lang = new string[] { @"""zh-CN""" };
-
-                    if (propertyGroup.Element("Authors") is { } author)
-                        meta.Author = author.Value;
-                    else
-                        throw new Exception($"{fileName}文件中缺少插件元数据<Authors><Authors/>");
-                    if (propertyGroup.Element("Description") is { } description)
-                        meta.Description = description.Value;
-                    else
-                        meta.Description = "";
-                    if (propertyGroup.Element("RepositoryUrl") is { } url)
-                        meta.WebUri = url.Value;
-                    else
-                        meta.WebUri = "";
-                    meta.MinVersion = GetMinVersion(context);
-                    var res = $@"PluginMetaData(
-    ""{meta.Id}"",
-    ""{meta.Name}"",
-    ""{meta.Description}"",
-    ""{meta.Author}"", ""{meta.Version}"",
-    ""{meta.WebUri}"",
-    ""{meta.Logo}"",
-    ""{meta.MinVersion}"", 
-    new string[]{{{GetRequire(context)}}},
-    new string[]{{{string.Join(",", meta.Lang)}}})";
-                    return res;
-                }
-
-                throw new Exception("未在当前项目的.csproj文件中找到PropertyGroup");
-            }
-        }
-
-        throw new Exception(@"缺少当前项目的.csproj文件,请将以下填入到.csproj文件中
-<ItemGroup>
-	<AdditionalFiles Include=""你的项目.csproj"" CopyToOutputDirectory=""PreserveNewest"" />
-</ItemGroup>");
     }
 
     public void Execute(GeneratorExecutionContext context)
@@ -156,13 +72,21 @@ internal class PluginMetaGenerator : ISourceGenerator
                     var classSymbol = model.GetDeclaredSymbol(cls);
 
                     // Check if the class has the Serializable attribute
-                    if (classSymbol.GetAttributes().Any(a =>
-                            a.AttributeClass.Equals(serializableSymbol, SymbolEqualityComparer.Default)))
+                    if (classSymbol!.GetAttributes().Any(a =>
+                            a.AttributeClass!.Equals(serializableSymbol, SymbolEqualityComparer.Default)))
                     {
-                        var meta = GetMeta(context);
+                        var np = PluginDNode!.Value<string>("Namespace");
+                        var metaType = PluginDNode!.Value<string>("Type");
+                        var attrs = new List<string>();
+                        foreach (var attr in PluginNode!)
+                        {
+                            attrs.Add($"{attr.Key} = {attr.Value}");
+                        }
+                        var meta = $"{metaType}({string.Join(",", attrs)})";
+
                         // Generate a Hello method with the class name
                         var code = $@"
-using ShadowViewer.Plugins;
+using {np};
 // Automatic Generate From ShadowPluginLoader.SourceGenerator
 namespace {classSymbol.ContainingNamespace.ToDisplayString()}
 {{
@@ -172,7 +96,7 @@ namespace {classSymbol.ContainingNamespace.ToDisplayString()}
         /// <summary>
         /// PluginMetaData
         /// </summary>
-        public static PluginMetaData Meta {{ get; }} = new {meta};
+        public static {metaType} Meta {{ get; }} = new {meta};
     }}
 }}";
                         // Add the generated code to the compilation
@@ -184,7 +108,8 @@ namespace {classSymbol.ContainingNamespace.ToDisplayString()}
         }
         catch (Exception e)
         {
-            LogError(context, e);
+            Logger.Error(context, e.Message);
+            throw e;
         }
     }
 
